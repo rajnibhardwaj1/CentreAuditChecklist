edbbd9cd21795af1fba4c151410c919b274626f1
rohityadavcs19 <61413373+rohityadavcs19@users.noreply.github.com>
2022-03-10 14:34:53 +0530

    Add files via upload

---
+++ lab_details.js
@@ -0,0 +1,23 @@
+let btnAdd = document.querySelector('button');
+let table = document.querySelector('table');
+
+
+let labInput = document.querySelector('#lab');
+let systemInput = document.querySelector('#system');
+let configInput = document.querySelector('#config');
+
+
+btnAdd.addEventListener('click', () => {
+    let lab = labInput.value;
+    let system = systemInput.value;
+    let configuration = configInput.value;
+
+    let template = `
+                <tr>
+                    <td>${lab}</td>
+                    <td>${system}</td>
+                    <td>${config}</td>
+                </tr>`;
+
+    table.innerHTML += template;
+});
