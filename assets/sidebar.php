07db178b91992ee51a3447d8984693a2294bfdee
rohityadavcs19 <61413373+rohityadavcs19@users.noreply.github.com>
2022-03-30 10:47:16 +0530

    Add files via upload

---
+++ assets/sidebar.php
@@ -0,0 +1,51 @@
+<?php?>
+<nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Test-Centre Audit</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Forms</p>
+                <li class="active">
+                    <a href="./Basic_details.php" data-toggle="collapse" aria-expanded="false" >Basic details</a>
+                    <!-- <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="">Detail 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Detail 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Detail 3</a>
+                        </li>
+                    </ul> -->
+                </li>
+                <li>
+                    <a href="./Desktop_config.php">Desktop Configuration</a>
+                </li>
+                <li>
+                <a href="./Power_backup.php">Power backup</a>
+                </li>
+                <li>
+                    <a href="./Internet_line1.php">Internet Line</a>
+                </li>
+                <li>
+                    <a href="./Backup_internet.php">Backup internet </a>
+                </li>
+                <li>
+                    <a href="./Network_details.php">Network Details</a>
+                </li>
+                <li>
+                    <a href="./Other_info.php">Other Information</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="#" class="download">SAVE</a>
+                </li>
+                <!-- <li>
+                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                </li> -->
+            </ul>
+        </nav>
