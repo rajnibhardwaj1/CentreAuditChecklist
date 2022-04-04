926bc4eb7b5b1ed5522898d58aa6987ada3c77a2
rohityadavcs19 <61413373+rohityadavcs19@users.noreply.github.com>
2022-04-04 13:57:53 +0530

    Add files via upload

--- registration/login.php
+++ registration/login.php
@@ -13,6 +13,8 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
+	$login_date=date("y-m-d");
+	$login_time=time("H:i:s");
 
 	if (empty($uname)) {
 		header("Location: index.php?error=User Name is required");
@@ -22,8 +24,10 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	    exit();
 	}else{
 		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		// $sql2= "INSERT INTO log_details (`id`,`user_name`,`login_time`,`login_date`,`logout_date`,`logout_time`) value (null,`$_SESSION[uname]`,'$login_date','$login_time')"
 
 		$result = mysqli_query($conn, $sql);
+		// $result2 = mysqli_query($conn, $sql2);
 
 		if (mysqli_num_rows($result) ===1 ) {
             //echo "hello";
@@ -34,6 +38,10 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
                 $_SESSION['user_name'] = $row['user_name'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
+
+				// $sql2= "INSERT INTO log_details (`id`,`user_name`,`login_time`,`login_date`,`logout_date`,`logout_time`) value (null,`$_SESSION[uname]`,'$login_date','$login_time')"
+				// $result2 = mysqli_query($conn, $sql2);
+				// $row = mysqli_fetch_assoc($result2);
             	header("Location: home.php");
 		        exit();
             }else{
